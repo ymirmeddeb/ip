d8403ec3e331ea8c85235c20d6b32305fe2f6279
ymirmeddeb <meddebymir@gmail.com>
2024-02-02 14:55:41 +0800

    changed the name of the chatbot and the message it prints out.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ╔╦═╦══╦═╦╗\n" +
+                      " ║╚╗║╔╗║═╣║\n" +
+                      " ║╔╣║╚╝║═╣║\n" +
+                      " ║╚═╩══╩═╝║\n" +
+                      " ╚════════╝\n";
+        System.out.println("Hello, I'm \n" + logo );
+        System.out.println("What can I do for you? \n");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }

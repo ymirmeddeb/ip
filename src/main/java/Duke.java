6338c5b6dd56802a66fa25f02d4193996c5ac1e2
ymirmeddeb <meddebymir@gmail.com>
2024-02-08 17:19:32 +0800

    added echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ╔╦═╦══╦═╦╗\n" +
@@ -5,8 +8,23 @@ public class Duke {
                       " ║╔╣║╚╝║═╣║\n" +
                       " ║╚═╩══╩═╝║\n" +
                       " ╚════════╝\n";
-        System.out.println("Hello, I'm \n" + logo );
-        System.out.println("What can I do for you? \n");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Hiiii, my name is\n" + logo );
+        System.out.println("I am here to help :3\n");
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Yes darling, what can i do for you?\n");
+        line = in.nextLine();
+        while (!line.equals("bye")){
+            echo(line);
+            System.out.print("Yes darling, what can i do for you?\n");
+            line = in.nextLine();
+        }
+        System.out.println("Love ya <3 See ya!!\n");
     }
+
+    public static void echo(String word){
+        System.out.println(word);
+        return;
+    }
+
 }

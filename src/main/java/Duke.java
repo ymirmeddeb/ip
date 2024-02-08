6f4e4540f8b77aaa8392974ad43741c92f566712
ymirmeddeb <meddebymir@gmail.com>
2024-02-08 18:55:29 +0800

    added functionality to add tasks to a list and display them

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,11 +11,20 @@ public class Duke {
         System.out.println("Hiiii, my name is\n" + logo );
         System.out.println("I am here to help :3\n");
         String line;
+        String[] tasks = new String[100];
+        int nextTask = 0;
         Scanner in = new Scanner(System.in);
         System.out.print("Yes darling, what can i do for you?\n");
         line = in.nextLine();
         while (!line.equals("bye")){
-            echo(line);
+            if (line.equals("list")){
+                printTasks(tasks, nextTask);
+            }
+            if (!line.equals("list")) {
+                tasks[nextTask] = line;
+                nextTask++;
+                System.out.println("I just added " + line + " to your list!!!");
+            }
             System.out.print("Yes darling, what can i do for you?\n");
             line = in.nextLine();
         }
@@ -25,6 +34,16 @@ public class Duke {
     public static void echo(String word){
         System.out.println(word);
         return;
+    }
+
+    public static void printTasks(String[] tasks, int nextTask){
+        if (tasks[0] == null){
+            System.out.println("Your list is empty ;_;");
+            return;
+        }
+        for (int i = 0; i < nextTask; i++) {
+            System.out.println(i+1 + ".   " + tasks[i] + "\n");
+        }
     }
 
 }
